c5ac2d6a8c328d283a809cff1db77c4cd2e36b6a
Veronika Ozretic <vozret00@fesb.hr>
2021-04-15 09:58:44 +0200

    outputting Dynamic data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,18 @@
 import './ExpenseItem.css';
+import React from "react";
 
 function ExpenseItem() {
+    const expenseDate = new Date(2021, 2, 27);
+    const expenseTitle = "Car Insurance";
+    const expenseAmount = 294.67;
+
     return(
         <div className="expense-item">
-            <div>March 28th 2021</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
-                <h2 >Car Insurance</h2>
+                <h2>{expenseTitle}</h2>
                 <div className="expense-item__price">
-                    $294.67
+                    ${expenseAmount}
                 </div>
             </div>
         </div>
