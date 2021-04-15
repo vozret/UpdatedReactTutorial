4b52d18cca90585aaf68ab6e198e5e0c02822ac4
Veronika Ozretic <vozret00@fesb.hr>
2021-04-15 09:49:18 +0200

    adding basic CSS styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,16 @@
+import './ExpenseItem.css';
 
 function ExpenseItem() {
     return(
-        <h2>Expense Item!</h2>
+        <div className="expense-item">
+            <div>March 28th 2021</div>
+            <div className="expense-item__description">
+                <h2 >Car Insurance</h2>
+                <div className="expense-item__price">
+                    $294.67
+                </div>
+            </div>
+        </div>
     );
 }
 
