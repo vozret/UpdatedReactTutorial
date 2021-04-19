251b433254f22e0fdda44fa232e49a04ea5ad937
Veronika Ozretic <vozret00@fesb.hr>
2021-04-19 12:39:19 +0200

    section 5: 62, 63, 64, 65, and 66

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,21 +2,36 @@ import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
+import "./Expenses.css";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2019");
 
-  const totalExpenses = props.expenses.map((item) => (
-    <ExpenseItem
-      key={item.id}
-      title={item.title}
-      amount={item.amount}
-      date={item.date}
-    ></ExpenseItem>
-  ));
+  const totalExpenses = props.expenses
+    .filter(
+      // eslint-disable-next-line eqeqeq
+      (item) => item.date.getFullYear() == filteredYear
+    )
+    .map((item) => (
+      <ExpenseItem
+        key={item.id}
+        title={item.title}
+        amount={item.amount}
+        date={item.date}
+      />
+    ));
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
+
+    //    const year = props.date.getFullYear();
+
+    // const filteredExpenses = props.expenses.filter(
+    //   // eslint-disable-next-line eqeqeq
+    //   (item) => item.date.getFullYear() == selectedYear
+    // );
+
+    // console.log(filteredExpenses);
   };
 
   return (
@@ -26,7 +41,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {totalExpenses}
+        {totalExpenses.length === 0 ? <p>No expenses found</p> : totalExpenses}
       </Card>
     </div>
   );
