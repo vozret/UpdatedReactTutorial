8d8816d3726d7a780cc066b41e5d7ad581d60293
Veronika <37539964+vozret@users.noreply.github.com>
2021-04-20 18:04:46 +0200

    Add files via upload
    uploading the src folder

---
+++ Practice-project/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (username, userAge) => {
+    setUsersList((prevState) => {
+      return [
+        ...prevState,
+        { name: username, age: userAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
